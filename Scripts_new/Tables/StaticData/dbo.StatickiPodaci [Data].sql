1043830c9fc445e621c261554ad7d77bf259683e
poticnenad <poticnenad@gmail.com>
2017-11-07 22:33:25 +0100

    Initial

---
+++ Scripts_new/Tables/StaticData/dbo.StatickiPodaci [Data].sql
@@ -0,0 +1,4 @@
+INSERT [dbo].[StatickiPodaci] ([statickiPodaciID], [StatickiPodaciInt], [StatickiPodaciDate], [StatickiPodaciVarChar]) VALUES (0, 1, CAST(N'2017-01-01' AS Date), N'test')
+INSERT [dbo].[StatickiPodaci] ([statickiPodaciID], [StatickiPodaciInt], [StatickiPodaciDate], [StatickiPodaciVarChar]) VALUES (1, 2, CAST(N'2017-01-02' AS Date), N'test2')
+INSERT [dbo].[StatickiPodaci] ([statickiPodaciID], [StatickiPodaciInt], [StatickiPodaciDate], [StatickiPodaciVarChar]) VALUES (2, 3, CAST(N'2017-01-03' AS Date), N'test3')
+INSERT [dbo].[StatickiPodaci] ([statickiPodaciID], [StatickiPodaciInt], [StatickiPodaciDate], [StatickiPodaciVarChar]) VALUES (3, 4, CAST(N'2017-01-04' AS Date), N'test4')
