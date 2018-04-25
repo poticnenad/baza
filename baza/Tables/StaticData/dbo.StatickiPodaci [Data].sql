9ec3ce7eaaf9d8d83af6a004fca42997e17e91ef
poticnenad@gmail.com <>
2018-04-25 04:05:24 -0700

    izmenjeno

--- baza/Tables/StaticData/dbo.StatickiPodaci [Data].sql
+++ baza/Tables/StaticData/dbo.StatickiPodaci [Data].sql
@@ -3,4 +3,4 @@ INSERT [dbo].[StatickiPodaci] ([statickiPodaciID], [StatickiPodaciInt], [Statick
 INSERT [dbo].[StatickiPodaci] ([statickiPodaciID], [StatickiPodaciInt], [StatickiPodaciDate], [StatickiPodaciVarChar]) VALUES (2, 3, CAST(N'2017-01-03' AS Date), N'test3')
 INSERT [dbo].[StatickiPodaci] ([statickiPodaciID], [StatickiPodaciInt], [StatickiPodaciDate], [StatickiPodaciVarChar]) VALUES (3, 4, CAST(N'2017-01-04' AS Date), N'test4')
 INSERT [dbo].[StatickiPodaci] ([statickiPodaciID], [StatickiPodaciInt], [StatickiPodaciDate], [StatickiPodaciVarChar]) VALUES (4, 5, CAST(N'2017-01-05' AS Date), N'test5')
-INSERT [dbo].[StatickiPodaci] ([statickiPodaciID], [StatickiPodaciInt], [StatickiPodaciDate], [StatickiPodaciVarChar]) VALUES (5, 6, CAST(N'2017-01-06' AS Date), N'test6')
+INSERT [dbo].[StatickiPodaci] ([statickiPodaciID], [StatickiPodaciInt], [StatickiPodaciDate], [StatickiPodaciVarChar]) VALUES (5, 6, CAST(N'2017-01-06' AS Date), N'izmenjeno')
